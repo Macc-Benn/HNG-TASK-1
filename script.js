56bb071be558ed50d18d0ab341854e39b9ddaeb3
Mfoniso Ndaobong <mfoniso.ndaobong@gmail.com>
2024-07-03 20:25:45 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function updateTimeAndDay() {
+    const now = new Date();
+    const utcTime = now.toUTCString().split(' ')[4];
+    const dayOfWeek = now.toLocaleDateString('en-US', { weekday: 'long' });
+
+    document.querySelector('[data-testid="currentTimeUTC"]').innerText = utcTime;
+    document.querySelector('[data-testid="currentDay"]').innerText = dayOfWeek;
+}
+
+setInterval(updateTimeAndDay, 1000);
+updateTimeAndDay();

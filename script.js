5e9b097f3dcc2325bf6468db11bb4732ddda94d1
Akem-Ben <akemini.ndaobong@gmail.com>
2024-07-05 12:37:49 +0100

    responsiveness and bug fixes

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// function updateTimeAndDay() {
-//     const now = new Date();
-//     const utcTime = now.toUTCString().split(' ')[4];
-//     const dayOfWeek = now.toLocaleDateString('en-US', { weekday: 'long' });
+function updateTimeAndDay() {
+    const now = new Date();
+    const utcTime = now.toUTCString().split(' ')[4];
+    const dayOfWeek = now.toLocaleDateString('en-US', { weekday: 'long' });
 
-//     document.querySelector('[data-testid="currentTimeUTC"]').innerText = utcTime;
-//     document.querySelector('[data-testid="currentDay"]').innerText = dayOfWeek;
-// }
+    document.querySelector('[data-testid="currentTimeUTC"]').innerText = utcTime;
+    document.querySelector('[data-testid="currentDay"]').innerText = dayOfWeek;
+}
 
-// setInterval(updateTimeAndDay, 1000);
-// updateTimeAndDay();
+setInterval(updateTimeAndDay, 1000);
+updateTimeAndDay();
 
+document.getElementById('menubutton').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('show');
+});

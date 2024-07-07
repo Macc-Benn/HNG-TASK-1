97dfbb144df91372a892e0395369dc4c9c01081a
Akem-Ben <akemini.ndaobong@gmail.com>
2024-07-07 23:37:45 +0100

    Fixed bugs and Responsivess

--- script.js
+++ script.js
@@ -5,17 +5,33 @@ function updateTimeAndDay() {
 
     document.querySelector('[data-testid="currentTimeUTC"]').innerText = utcTime;
     document.querySelector('[data-testid="currentDay"]').innerText = dayOfWeek;
+
+    document.querySelector('[data-testid="currentTimeUTC1"]').innerText = utcTime;
+    document.querySelector('[data-testid="currentDay1"]').innerText = dayOfWeek;
+
 }
 
 setInterval(updateTimeAndDay, 1000);
 updateTimeAndDay();
 
 function openNav() {
-    document.getElementById("topmenubar").style.width = "150px";
+    
+    document.getElementById("topmenubar").classList.remove('hidden-menu-bar').style.width = "150px";
     document.querySelector(".main").style.marginLeft = "150px";
 }
 
 function closeNav() {
-    document.getElementById("topmenubar").style.width = "0";
+    document.getElementById("topmenubar").classList.add('hidden-menu-bar').style.width = "0";
     document.querySelector(".main").style.marginLeft = "0";
 }
+
+document.getElementById('openPopupButton').addEventListener('click', function() {
+    document.getElementById('popup').classList.remove('hidden');
+});
+document.getElementById('openPopupButton2').addEventListener('click', function() {
+    document.getElementById('popup').classList.remove('hidden');
+});
+
+document.getElementById('closePopupButton').addEventListener('click', function() {
+    document.getElementById('popup').classList.add('hidden');
+});
